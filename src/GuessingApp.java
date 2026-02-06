f7614f109e56984cc14dc0ded31c153d055c5496
PeKkA01 <agulati395@gmail.com>
2026-02-06 17:57:23 +0530

    UC-6 done

--- src/GuessingApp.java
+++ src/GuessingApp.java
@@ -7,64 +7,81 @@ public class GuessingApp {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter player name: ");
+        System.out.print("Enter player name: ");
         String playerName = sc.nextLine();
 
-        System.out.println("\nWelcome to the Guessing App");
-        System.out.println("============================");
+        boolean playAgain = true;
 
-        GameConfig gcon = new GameConfig();
-        gcon.showRule();
+        while (playAgain) {
 
-        int attempt = 0;
-        int hintsUsed = 0;
-        boolean guessedCorrect = false;
+            System.out.println("\nWelcome to the Guessing App");
+            System.out.println("============================");
 
-        while (attempt < gcon.getMax_Attempts()) {
+            GameConfig gcon = new GameConfig();
+            gcon.showRule();
 
-            try {
-                System.out.print("Enter your guess: ");
+            int attempt = 0;
+            int hintsUsed = 0;
+            boolean guessedCorrect = false;
 
-                int guess = GuessValidator.getValidGuess(sc, 1, 100);
-                attempt++;
+            while (attempt < gcon.getMax_Attempts()) {
 
-                int result = GuessValidator.compareGuess(
-                        guess, gcon.getTargetNumber());
+                try {
+                    System.out.print("Enter your guess: ");
 
-                if (result == 0) {
-                    System.out.println("Correct! You guessed in "
-                            + attempt + " attempts");
-                    guessedCorrect = true;
-                    break;
+                    int guess = GuessValidator.getValidGuess(sc, 1, 100);
+                    attempt++;
+
+                    int result = GuessValidator.compareGuess(
+                            guess, gcon.getTargetNumber());
+
+                    if (result == 0) {
+                        System.out.println("Correct! You guessed in "
+                                + attempt + " attempts");
+                        guessedCorrect = true;
+                        break;
+                    }
+                    else if (result == -1) {
+                        System.out.println("Too LOW! Try higher.");
+                    }
+                    else {
+                        System.out.println("Too HIGH! Try lower.");
+                    }
+
+                    // hint
+                    if (!guessedCorrect && hintsUsed < gcon.getMax_Hints()) {
+                        hintsUsed++;
+                        String hint = HintService.generation(
+                                gcon.getTargetNumber(), hintsUsed);
+                        System.out.println(hint);
+                    }
+
+                } catch (InvalidGuessException e) {
+                    System.out.println(e.getMessage());
                 }
-                else if (result == -1) {
-                    System.out.println("Too LOW! Try higher.");
-                }
-                else {
-                    System.out.println("Too HIGH! Try lower.");
-                }
+            }
 
-                if (!guessedCorrect && hintsUsed < gcon.getMax_Hints()) {
-                    hintsUsed++;
-                    String hint = HintService.generation(
-                            gcon.getTargetNumber(), hintsUsed);
-                    System.out.println(hint);
-                }
+            if (!guessedCorrect) {
+                System.out.println("Game Over!");
+                System.out.println("Correct number was: "
+                        + gcon.getTargetNumber());
+            }
 
-            } catch (InvalidGuessException e) {
-                System.out.println(e.getMessage());
+            // save result
+            StorageService.saveResult(playerName, attempt, guessedCorrect);
+
+            // show history
+            StorageService.showResults();
+
+            // restart option
+            System.out.print("\nDo you want to play again? (yes/no): ");
+            String choice = sc.next();
+
+            if (!choice.equalsIgnoreCase("yes")) {
+                playAgain = false;
+                System.out.println("Thanks for playing! Exiting game...");
             }
         }
-
-        if (!guessedCorrect) {
-            System.out.println("Game Over!");
-            System.out.println("Correct number was: "
-                    + gcon.getTargetNumber());
-        }
-
-        StorageService.saveResult(playerName, attempt, guessedCorrect);
-
-        StorageService.showResults();
 
         sc.close();
     }
